97709f1d1707a9c60570db3d4d12be64a61ca55f
sinni <aarna.si09@gmail.com>
2024-06-18 12:03:39 +0530

    Adds plot to flit line over the train data.

--- linear_regression/simple_linear_regression.py
+++ linear_regression/simple_linear_regression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
 
 # Reading the dataset from the given URL
 # This dataset contains various features of cars, including engine size and CO2 emissions
@@ -31,6 +32,13 @@ linear_regression.fit(train_x, train_y)
 print('Train: Coefficients:', linear_regression.coef_)
 print('Train: Intercept:', linear_regression.intercept_)
 
+# Plotting the fit line over the train data
+plt.scatter(train_x, train_y, color='blue')
+plt.plot(train_x, linear_regression.coef_[0][0] * train_x + linear_regression.intercept_[0], '-r')
+plt.xlabel("Engine Size")
+plt.ylabel("Emission")
+plt.show()
+
 # Preparing the testing data
 test_x = np.asanyarray(test[['ENGINESIZE']])  # Features
 test_y = np.asanyarray(test[['CO2EMISSIONS']])  # Target variable
@@ -39,6 +47,7 @@ test_y = np.asanyarray(test[['CO2EMISSIONS']])  # Target variable
 predict_y = linear_regression.predict(test_x)
 
 # Evaluating the model performance on the testing data
+print("Computing model performance")
 print("Test:Mean aboslute error: %.2f" % np.mean(np.absolute(test_y - predict_y)))
 print("Test: Residual sum of squares (MSE): %.2f" % np.mean(np.square(test_y - predict_y)))
 print("Test: R2-score: %.2f" % r2_score(test_y, predict_y))
